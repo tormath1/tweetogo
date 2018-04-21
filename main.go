f330616addbd2d5e08ac011f4965774f7f43843c
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-04-21 19:19:17 +0200

    feat(main): load all api keys

--- main.go
+++ main.go
@@ -6,16 +6,28 @@ import (
 )
 
 func main() {
-	consumerKey, err := ioutil.ReadFile("./consumer.key")
+	consumerKey, err := ioutil.ReadFile("./secrets/consumer-key")
 	if err != nil {
 		log.Fatal("Error while loading consumer key.\n", err)
 	}
 
-	consumerSecret, err := ioutil.ReadFile("./consumer.secret")
+	consumerSecret, err := ioutil.ReadFile("./secrets/consumer-secret")
 	if err != nil {
 		log.Fatal("Error while loading consumer secret.\n", err)
 	}
 
+	accessToken, err := ioutil.ReadFile("./secrets/access-token")
+	if err != nil {
+		log.Fatal("Error while loading access token.\n", err)
+	}
+
+	accessTokenSecret, err := ioutil.ReadFile("./secrets/access-token-secret")
+	if err != nil {
+		log.Fatal("Error while loading access token secret.\n", err)
+	}
+
 	log.Print(string(consumerKey))
 	log.Print(string(consumerSecret))
+	log.Print(string(accessTokenSecret))
+	log.Print(string(accessToken))
 }
