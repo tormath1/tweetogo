5e3de26cd0dc91fa2b5cbcc3f0efb54d0751660f
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-04-22 14:34:08 +0200

    feat(telegram/main): implement telegram bot api

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
+	"gopkg.in/telegram-bot-api.v4"
 )
 
 func main() {
@@ -33,6 +35,11 @@ func main() {
 		log.Fatal("Error while loading access token secret.\n", err)
 	}
 
+	telegramToken, err := ioutil.ReadFile("./secrets/telegram-token")
+	if err != nil {
+		log.Fatal("Error while loading telegram token.\n", err)
+	}
+
 	config := oauth1.NewConfig(string(consumerKey), string(consumerSecret))
 	token := oauth1.NewToken(string(accessToken), string(accessTokenSecret))
 
@@ -55,4 +62,45 @@ func main() {
 	for _, tweet := range tweets.Statuses {
 		log.Print(tweet.Text, tweet.Entities.Urls)
 	}
+
+	bot, err := tgbotapi.NewBotAPI(string(telegramToken))
+	if err != nil {
+		log.Fatal("Error while creating new Telegram bot client. ", err)
+	}
+
+	log.Print(bot.Self.UserName)
+
+	update := tgbotapi.UpdateConfig{}
+	update.Timeout = 60
+	update.Offset = 295741457
+
+	updates, err := bot.GetUpdates(update)
+	if err != nil {
+		log.Fatal("Error while fetching last updates. ", err)
+	}
+
+	for _, u := range updates {
+		if u.Message == nil {
+			continue
+		}
+		log.Printf("[%s] Update ID: %d", u.Message.Text, u.UpdateID)
+		if err = popular(bot, &u, u.Message.Text); err != nil {
+			log.Print(err)
+		}
+	}
+}
+
+func help(bot *tgbotapi.BotAPI, update *tgbotapi.Update) error {
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "/popular word : get last popular tweets about `word`")
+	_, err := bot.Send(msg)
+	return err
+}
+
+func popular(bot *tgbotapi.BotAPI, update *tgbotapi.Update, text string) error {
+	if len(strings.Split(text, " ")) != 2 {
+		return help(bot, update)
+	}
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Popular: %s", strings.Split(text, " ")[1]))
+	_, err := bot.Send(msg)
+	return err
 }
